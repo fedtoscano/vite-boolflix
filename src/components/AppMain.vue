211a2b857b3e3e1e7814a8ee315b6116de49ac6c
fedtoscano <federicotoscano611@gmail.com>
2024-06-14 11:38:23 +0200

    scaffolding iniziale

---
+++ src/components/AppMain.vue
@@ -0,0 +1,24 @@
+<script>
+import AppSearch from "./AppSearch.vue"
+import AppMovieList from "./AppMovieList.vue"
+
+export default {
+
+components: {
+    AppSearch,
+    AppMovieList,
+},
+
+data() {
+return {
+}
+}}
+</script>
+
+<template>
+    <AppSearch/>
+    <AppMovieList/>
+</template>
+
+<style scoped>
+</style>
