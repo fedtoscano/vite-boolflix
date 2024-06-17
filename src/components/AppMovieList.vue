4f71a482a5426450a62a7ef9cc39d639e11ee08b
fedtoscano <federicotoscano611@gmail.com>
2024-06-17 10:33:46 +0200

    implementate correttamente le bandierine per le lingue

--- src/components/AppMovieList.vue
+++ src/components/AppMovieList.vue
@@ -17,14 +17,15 @@ computer(){
         <li v-for="movie in store.movieList">
             <p>{{ movie.title }}</p>
             <p>{{ movie.original_title }}</p>
-            <p>{{ movie.original_language }}</p>
+            <span class="lang-icon" :class="`lang-icon-${movie.original_language}`"></span>
             <p>{{ movie.vote_average }}</p>
-            <span class="lang-icon lang-icon-en"></span>
         </li>
     </ul>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+@use "../style/flag-icons.scss" as *;
+
 ul{
     list-style-type: none;
 }
